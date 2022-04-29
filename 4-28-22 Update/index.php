143f323af71ef9c75dfc5971a44b5ab893a24844
itshawnpark <sparkjy20@gmail.com>
2022-04-29 09:25:11 -0500

    Update index.php

--- 4-28-22 Update/index.php
+++ 4-28-22 Update/index.php
@@ -36,8 +36,7 @@ if(isset($_COOKIE['username']) && isset($_COOKIE['password'])){
                 <a id="navmiddle" href="<?php if ($active == true) { echo "./meta.html ";} else {echo $whereTo;} ?>">METAVERSE</a>
                 <a id="navright" href="<?php if ($active == true) { echo "./nft.html ";} else {echo $whereTo;} ?>">NFTs</a>
             </div>
-            <h1>What is Web3?</h1>
-            <h3>Overview</h3>
+            <h3>What is Web3?</h3>
             <div class="h3info">
                 <ul>
                     <li>Web3 is a term used to refer to the next iteration of the internet.</li>
@@ -58,6 +57,14 @@ if(isset($_COOKIE['username']) && isset($_COOKIE['password'])){
                 <button onClick="window.location.href = 'https://www.sandbox.game/en//'">Sandbox Metaverse</button>
                 <button onClick="window.location.href = 'https://coinmarketcap.com/'">Crypto Exchange</button>
             </div>
+
+            <div style="height:433px; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; 
+border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; font-size: 12px; 
+font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #56667F; padding: 0px; margin: 0px; width: 100%;">
+<div style="height:413px; padding:0px; margin:0px; width: 100%;"><iframe src="https://widget.coinlib.io/widget?type=full_v2&theme=light&cnt=6&pref_coin_id=1505&graph=yes" width="100%" height="409px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe></div>
+<div style="color: #FFFFFF; line-height: 14px; font-weight: 400; font-size: 11px; box-sizing: border-box; padding: 2px 6px; width: 100%; font-family: Verdana, Tahoma, Arial, sans-serif;">
+    <a href="https://coinlib.io" target="_blank" style="font-weight: 500; color: #FFFFFF; text-decoration:none; font-size:11px">Cryptocurrency Prices</a>&nbsp;by Coinlib</div>
+</div>
         </div>
         <footer>
             <div id="footinfo">
